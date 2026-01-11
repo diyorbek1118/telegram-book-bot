399446e05f5e962d2dd1878f51f707faa19a0504
diyor <diyor@debian.debian>
2026-01-11 21:31:46 +0500

    add docker file

--- public/index.php
+++ public/index.php
@@ -1,23 +1,22 @@
 <?php
-
 require __DIR__ . '/../vendor/autoload.php';
 
 use Telegram\Bot\Api;
 
-$telegram = new Api($_ENV['BOT_TOKEN']);
+$telegram = new Api($_ENV['BOT_TOKEN'] ?? '');
+
+file_put_contents('log.txt', date('Y-m-d H:i:s') . " - Webhook hit\n", FILE_APPEND);
 
 $update = $telegram->getWebhookUpdate();
 
 if ($update->getMessage()) {
-
-    $message = $update->getMessage();
-    $chatId  = $message->getChat()->getId();
-    $text    = $message->getText();
+    $chatId = $update->getMessage()->getChat()->getId();
+    $text = $update->getMessage()->getText();
 
     if ($text === '/start') {
         $telegram->sendMessage([
             'chat_id' => $chatId,
-            'text' => "✅ Railway PHP bot ishlayapti!\n/start komandasi qabul qilindi"
+            'text' => "✅ Bot ishlayapti!\n/start komandasi qabul qilindi"
         ]);
     }
 }
